fb2c10ad2656d93455aef04956f92ad51588c155
CristhianRodriguezMolina <camilo.r13@hotmail.com>
2021-12-06 16:38:49 -0500

    Associated categories to the product in catalog context

--- hello/lib/hello/catalog/product.ex
+++ hello/lib/hello/catalog/product.ex
@@ -2,11 +2,17 @@ defmodule Hello.Catalog.Product do
   use Ecto.Schema
   import Ecto.Changeset
 
+  # Relations
+  alias Hello.Catalog.Category
+
   schema "products" do
-    field :description, :string
-    field :price, :decimal
-    field :title, :string
-    field :views, :integer
+    field(:description, :string)
+    field(:price, :decimal)
+    field(:title, :string)
+    field(:views, :integer)
+
+    # Association through the `product_categories` table
+    many_to_many(:categories, Category, join_through: "product_categories", on_replace: :delete)
 
     timestamps()
   end
